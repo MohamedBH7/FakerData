d1c6e13684cc9ed53f3eb65d30ba76a19430f3a2
Mohamed Alsaffar <malsaffar58@gmail.com>
2025-08-22 20:36:07 +0300

    HealthcarePredictionGenerator

---
+++ Faker.Generater/HealthcarePredictionGenerator.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+using System;
+using System.Collections.Generic;
+
+namespace Faker.Generater
+{
+    /// <summary>
+    /// Generates synthetic healthcare patient data for ML classification tasks.
+    /// </summary>
+    /// HealthcarePredictionGenerator_Documentation.markdown
+    public static class HealthcarePredictionGenerator
+    {
+        private static readonly Random _random = new Random();
+        private static readonly string[] Genders = { "Male", "Female" };
+
+        public class PatientData
+        {
+            public Guid PatientID { get; set; }
+            public int Age { get; set; }
+            public string Gender { get; set; }
+            public int BloodPressure { get; set; } // systolic mmHg
+            public int Cholesterol { get; set; }   // mg/dL
+            public bool HeartDisease { get; set; }
+        }
+
+        public static List<PatientData> GenerateData(int count = 100)
+        {
+            var data = new List<PatientData>();
+            for (int i = 0; i < count; i++)
+            {
+                data.Add(new PatientData
+                {
+                    PatientID = Guid.NewGuid(),
+                    Age = _random.Next(20, 90),
+                    Gender = Genders[_random.Next(Genders.Length)],
+                    BloodPressure = _random.Next(90, 180),
+                    Cholesterol = _random.Next(150, 300),
+                    HeartDisease = _random.NextDouble() > 0.8 // ~20% risk
+                });
+            }
+            return data;
+        }
+    }
+}
---
+++ Faker.Generater/HealthcarePredictionGenerator_Documentation.markdown
@@ -0,0 +1,24 @@
+﻿# HealthcarePredictionGenerator Documentation
+
+## Description
+Generates synthetic patient data for **ML classification** tasks such as predicting heart disease.
+
+## Class: HealthcarePredictionGenerator
+
+### Methods
+- `GenerateData(int count = 100)`: Returns a list of `PatientData` objects.
+
+### Data Fields
+
+| Field          | Type      | Description                           |
+|----------------|-----------|---------------------------------------|
+| PatientID      | Guid      | Unique patient identifier              |
+| Age            | int       | Patient age                            |
+| Gender         | string    | Gender (Male/Female)                  |
+| BloodPressure  | int       | Systolic blood pressure in mmHg        |
+| Cholesterol    | int       | Cholesterol level in mg/dL             |
+| HeartDisease   | bool      | True if patient has heart disease      |
+
+### Usage Example
+```csharp
+var patients = HealthcarePredictionGenerator.GenerateData(50);
